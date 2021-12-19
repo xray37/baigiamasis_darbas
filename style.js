86a89c39217e12d04df6721d8610220179e7aefd
xray37 <aizinas.ray@gmal.com>
2021-12-19 15:45:52 +0100

    header&hero section

---
+++ style.js
@@ -0,0 +1,14 @@
+"use strict"
+
+// menu_burgeris
+
+
+let menuTrigger = document.getElementById("mobile-menu-trigger");
+let mobileMenu = document.querySelector("header nav");
+
+menuTrigger.addEventListener('click', function() {
+  this.classList.toggle("active");
+  mobileMenu.classList.toggle("active");
+});
+
+
