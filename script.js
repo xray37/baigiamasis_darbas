6898b33b4ad2e43bcac50414d63afdfc6a9c9409
xray37 <aizinas.ray@gmal.com>
2021-12-21 21:03:14 +0100

    work in progress...(bet kazkaip viskas griuuuuna :(...)

--- script.js
+++ script.js
@@ -12,33 +12,76 @@ menuTrigger.addEventListener('click', function() {
 
 // features section tab navigation
 
+const tabNavigationElements = document.querySelectorAll(".about .tab-navigation li");
+const tabContentElements = document.querySelectorAll(".about .tab-content");
+
+function removeActiveClassesFromElements(elements) {
+    for (let element of elements) {
+        element.classList.remove("active");
+    }
+}
+
+for (let tabElement of tabNavigationElements) {
+    tabElement.addEventListener("click", function() {
+        const target = this.dataset.target; // "register" / "apply" / "receive"
+
+        removeActiveClassesFromElements(tabNavigationElements);
+        this.classList.add("active");
+
+        removeActiveClassesFromElements(tabContentElements);
+        document.querySelector('.tab-content[data-tab="'+target+'"]').classList.add('active');
+    });
+}
+
 
 
 
 // testimonials section swiper gallery
 
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-
-    // If we need pagination
-    pagination: {
+const swiper = new Swiper('.reviews-swiper', {
+  speed: 900,
+  slidesPerView: 1,
+  spaceBetween: 50,
+  // slidesPerGroup: 3,
+  autoHeight: true,
+  loop: true,
+  autoplay: {
+      delay: 2000,
+  },
+  pagination: {
       el: '.swiper-pagination',
-    },
-
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+      clickable: true
+  },
+  breakpoints: {
+      800: {
+        slidesPerView: 3,
+        spaceBetween: 120
+      }
+    }
+});
 
 //ORAI:
 // API key:
 // 3938ac6ab938c88279184afe6c675938 & units=metric;
+// Get weather data from openweather API
+
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Sint-Niklaas&units=metric&appid=3938ac6ab938c88279184afe6c675938";
+const weatherElement = document.getElementById("weather-in-celsius");
+
+function getCurrentWeatherInCelsius() {
+    const http = new XMLHttpRequest();
+    http.open("GET", url);
+    http.addEventListener('load', function() {
+        const response = JSON.parse(http.response);
+        const temperature = Math.round(response.main.temp);
+
+        if (temperature > 0) {
+            weatherElement.innerText = "+" + temperature;
+        } else {
+            weatherElement.innerText = temperature;
+        }
+    })
+    http.send();
+}
+
+window.addEventListener('load', getCurrentWeatherInCelsius)
